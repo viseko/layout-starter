63168a1983b1640f74c0f36a034efa8a040a1720
Viktor Konovalov <konovalov@design-lab.ru>
2023-07-23 23:12:14 +0700

    Clean trash

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,15 @@
 import "./modules/calc-scrollbar-width.js"; // Вычисление ширины  скроллбара и запись в css-переменную
-import "./modules/init-inputs.js"; // Иниц-я полей ввода
-import "./modules/select-fields.js"; // Иниц-я селектов
-import "./modules/youtube-video.js"; // Youtube-врезки
-import "./modules/resizable-textareas.js";
-import "./modules/mobile-menu.js"; 
-import "./modules/dropdown-position.js"; // Расчёт позиций dropdown-меню
-import "./modules/main-slider.js"; // Слайдер на главной
-import "./modules/content-slider.js"; // Слайдеры в статьях
-import "./modules/card-slider.js";
-import "./modules/account-nav.js"; // Мобильная навигация в ЛК
-import "./modules/text-truncate.js"; // Обрезка текста
-
-import "./modules/modals.js"; // Модальные и диалоговые окна
+// import "./modules/init-inputs.js"; // Иниц-я полей ввода
+// import "./modules/select-fields.js"; // Иниц-я селектов
+// import "./modules/youtube-video.js"; // Youtube-врезки
+// import "./modules/resizable-textareas.js";
+// import "./modules/mobile-menu.js"; 
+// import "./modules/dropdown-position.js"; // Расчёт позиций dropdown-меню
+// import "./modules/main-slider.js"; // Слайдер на главной
+// import "./modules/card-slider.js";
+// import "./modules/text-truncate.js"; // Обрезка текста
+// import "./modules/modals.js"; // Модальные и диалоговые окна
 // import "./modules/tooltips.js"; // Вспл. подсказки
-import "./modules/forms-validate.js"; // Валидация форм
-import "./modules/forms-submit.js"; // Асинхронная отправка форм
-
-
-import "simplebar"; // Кастомный скролл
+// import "./modules/forms-validate.js"; // Валидация форм
+// import "./modules/forms-submit.js"; // Асинхронная отправка форм
+// import "simplebar"; // Кастомный скролл
